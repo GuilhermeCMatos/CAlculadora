3df6781762020962b53c116d4610de61ff748406
Usuario <Usuario@CPD03-M18>
2024-04-08 14:02:50 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppCalc/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace CAlculadora
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ MauiAppCalc/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace CAlculadora
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
